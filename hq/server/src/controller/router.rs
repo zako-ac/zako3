7950ce9a9b4be6131f9707cb1ea4c0fed4c1b6e7
minco <mail@drchi.co.kr>
2025-10-15 18:13:14 +0900

    fix(hq): add missing routes

--- hq/server/src/controller/router.rs
+++ hq/server/src/controller/router.rs
@@ -6,10 +6,14 @@ use crate::{controller::routes::user::*, core::app::AppState};
 
 pub fn create_router(state: AppState) -> Router {
     let (router, openapi) = OpenApiRouter::new()
-        .routes(routes!(create_user))
+        .routes(routes!(
+            create_user,
+            get_user,
+            update_user_public,
+            update_user_permissions,
+            delete_user
+        ))
         .split_for_parts();
-
-    
 
     Router::new()
         .merge(SwaggerUi::new("/swagger-ui").url("/openapi.json", openapi))
