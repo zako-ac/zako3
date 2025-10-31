9961c4e3ca681433711f2fe7251513506bf62938
minco <mail@drchi.co.kr>
2025-10-31 13:41:43 +0900

    nix: add flake

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "Zako3 Flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          name = "zako3";
+          buildInputs = with pkgs; [ openssl pkg-config ];
+        };
+      });
+}
